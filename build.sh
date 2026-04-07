06f88e229324e76282f5539ca1fe4d3323e25515
Alvise Dorig <alvise72@live.com>
2026-04-07 22:37:33 +0200

    different builds

---
+++ build.sh
@@ -0,0 +1,5 @@
+cmake -S . -B build-release -DCMAKE_BUILD_TYPE=Release
+cmake --build build-release
+
+cmake -S . -B build-debug -DCMAKE_BUILD_TYPE=Debug
+cmake --build build-debug
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
 #include <QApplication>
+#include <QStyleFactory>
+#include <QDebug>
 
 #include "picalculatorwindow.h"
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
-
+    qDebug() << QStyleFactory::keys();
+    app.setStyle("windowsvista");
     PiCalculatorWindow window;
     window.show();
 
