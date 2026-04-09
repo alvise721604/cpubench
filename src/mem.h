0a84fed2e48d3b9a38648b7734db65ca800d024f
alvise721604 <alvise72@live.com>
2026-04-10 00:18:04 +0200

    Create mem.h

---
+++ src/mem.h
@@ -0,0 +1,11 @@
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <vector>
+
+namespace mem {
+
+    void mem_test_init( std::vector<char> &, const std::size_t );
+    void mem_test_write( std::vector<char> &, const unsigned int );
+
+}
