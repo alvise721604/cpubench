50769f360797b1cf8000c414e57cc86f5fa2f60c
alvise721604 <alvise72@live.com>
2026-04-10 00:32:56 +0200

    Update mem.cpp

--- src/mem.cpp
+++ src/mem.cpp
@@ -1,10 +1,10 @@
 #include "mem.h"
 
-void mem_test_init( std::vector<char> &buf, const std::size_t bufsize ) {
+void mem::mem_test_init( std::vector<char> &buf ) {
     std::memset(buf.data(), 2, buf.size());
 }
 
-void mem_test_write( std::vector<char> &buf, const unsigned int iterations ) {
+void mem::mem_test_write( std::vector<char> &buf, const unsigned int iterations ) {
     for (int i = 0; i < iterations; ++i) {
         std::memset(buf.data(), 0, buf.size());
     }
