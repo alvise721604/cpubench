b89d8e737e3c0cf1e0543605613316fdc97e2bad
alvise721604 <alvise72@live.com>
2026-04-10 23:33:57 +0200

    Update build-cli.sh

--- build-cli.sh
+++ build-cli.sh
@@ -1,1 +1,1 @@
-g++ -O3 -fopenmp -std=c++20 src/main_cli.cpp src/calc.cpp src/mem.cpp -o bench_cli
+/opt/homebrew/opt/gcc/bin/g++-15 -O3 -fopenmp -std=c++20 src/main_cli.cpp src/calc.cpp src/mem.cpp -o bench_cli
