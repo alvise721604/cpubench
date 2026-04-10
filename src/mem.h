a2bcf898a1ddb39814eaf46e32885fc10130b130
alvise721604 <alvise72@live.com>
2026-04-10 14:23:42 +0200

    Update mem.h

--- src/mem.h
+++ src/mem.h
@@ -5,5 +5,6 @@ namespace mem {
 
     void mem_test_init( std::vector<char> & );
     void mem_test_write( std::vector<char> &, const unsigned int );
+    void mem_test_write_omp(std::vector<char>&, const unsigned int);
 
 }
