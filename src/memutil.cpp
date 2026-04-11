5370f3e8bf04c2b6a8bcf83e35c8f4aa69d086b3
alvise721604 <alvise72@live.com>
2026-04-11 11:12:21 +0200

    Update memutil.cpp

--- src/memutil.cpp
+++ src/memutil.cpp
@@ -4,7 +4,7 @@
     #define NOMINMAX
     #include <windows.h>
 
-    std::uint64_t get_installed_ram_bytes() {
+    std::uint64_t memutil::get_installed_ram_bytes() {
         MEMORYSTATUSEX status{};
         status.dwLength = sizeof(status);
         if (GlobalMemoryStatusEx(&status)) {
@@ -17,7 +17,7 @@
     #include <sys/types.h>
     #include <sys/sysctl.h>
 
-    std::uint64_t get_installed_ram_bytes() {
+    std::uint64_t memutil::get_installed_ram_bytes() {
         std::uint64_t mem = 0;
         size_t len = sizeof(mem);
         if (sysctlbyname("hw.memsize", &mem, &len, nullptr, 0) == 0) {
@@ -29,7 +29,7 @@
 #elif defined(__linux__)
     #include <sys/sysinfo.h>
 
-    std::uint64_t get_installed_ram_bytes() {
+    std::uint64_t memutil::get_installed_ram_bytes() {
         struct sysinfo info {};
         if (sysinfo(&info) == 0) {
             return static_cast<std::uint64_t>(info.totalram) * info.mem_unit;
@@ -38,7 +38,7 @@
     }
 
 #else
-    std::uint64_t get_installed_ram_bytes() {
+    std::uint64_t memutil::get_installed_ram_bytes() {
         return 0; // non supportato
     }
 #endif
