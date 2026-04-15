3fa66077634a9c14f2946956c253e79649c000a5
alvise721604 <alvise72@live.com>
2026-04-15 17:53:49 +0200

    Update calc.cpp

--- src/calc.cpp
+++ src/calc.cpp
@@ -10,151 +10,111 @@
 namespace calc {
 
     /*
-        FABRICE BELLARD
-     */
-    double pi_fabrice_bellard(int iterations) {
-
-        //double sign = 1.0;
-        double result = 0.0;
-        double factor = 1.0;
-
-        for (int n = 0; n < iterations-1; n+=2) {
-            result += factor * (
-                1.0 / (10.0 * n + 9.0)
-                - 4.0 / (10.0 * n + 7.0)
-                - 4.0 / (10.0 * n + 5.0)
-                - 64.0 / (10.0 * n + 3.0)
-                + 256.0 / (10.0 * n + 1.0)
-                - 1.0 / (4.0 * n + 3.0)
-                - 32.0 / (4.0 * n + 1.0)
-            );
-            factor /= 1048576.0;
-        }
-        for (int n = 1; n < iterations-1; n+=2) {
-            result += -factor * (
-                1.0 / (10.0 * n + 9.0)
-                - 4.0 / (10.0 * n + 7.0)
-                - 4.0 / (10.0 * n + 5.0)
-                - 64.0 / (10.0 * n + 3.0)
-                + 256.0 / (10.0 * n + 1.0)
-                - 1.0 / (4.0 * n + 3.0)
-                - 32.0 / (4.0 * n + 1.0)
-            );
-            factor /= 1048576.0;
-        }
-
-        return result / 64.0;
-    }
-
-    /*
         Single-CPU LEIBNIZ
     */
-    double pi_leibniz(std::size_t iterations) {
+    long double pi_leibniz(const std::size_t iterations) {
         double sum = 0.0;
-        for (std::size_t n = 0; n < iterations; n+=2) {
-            sum += 1.0 / (2.0 * static_cast<double>(n) + 1.0);
+
+        for (std::size_t n = 0; n < iterations; ++n) {
+            //long double sign = (n % 2 == 0) ? 1.0 : -1.0;
+            long double sign = (n & 1) ? -1.0 : 1.0; // bitwise è più veloce
+            sum += sign / (2.0L * static_cast<double>(n) + 1.0L);
         }
-        for (std::size_t n = 1; n < iterations; n+=2) {
-            sum += (-1.0) / (2.0 * static_cast<double>(n) + 1.0);
-        }
-        return 4.0 * sum;
+        return 4.0L * sum;
     }
 
     /*
         OMP LEIBNIZ
     */
-    double pi_leibniz_omp(std::size_t iterations) {
-        double sum = 0.0;
+    long double pi_leibniz_omp( const  std::size_t iterations) {
+        long double sum = 0.0;
         #pragma omp parallel for simd reduction(+:sum)
-        for (unsigned long long n = 0; n < iterations; n+=2) {
-            sum += 1.0 / (2.0 * static_cast<double>(n) + 1.0);
+        for (std::size_t n = 0; n < iterations; ++n) {
+            //long double sign = (n % 2 == 0) ? 1.0 : -1.0;
+            long double sign = (n & 1) ? -1.0 : 1.0; // bitwise è più veloce
+            sum += sign / (2.0L * static_cast<long double>(n) + 1.0L);
         }
-        #pragma omp parallel for simd reduction(+:sum)
-        for (unsigned long long n = 1; n < iterations; n+=2) {
-            sum += (-1.0) / (2.0 * static_cast<double>(n) + 1.0);
-        }
-        return 4.0 * sum;
+        return 4.0L * sum;
     }
 
     /*
         Single-CPU EULER
     */
-    double pi_euler(std::size_t iterations) {
-        double sum = 0.0;
+    long double pi_euler( const std::size_t iterations) {
+        long double sum = 0.0;
         for (unsigned long long n = 1; n < iterations; ++n) {
-            const double d = static_cast<double>(n);
-            sum += 1.0 / (d * d);
+            const long double d = static_cast<long double>(n);
+            sum += 1.0L / (d * d);
         }
-        return std::sqrt(6.0 * sum);
+        return std::sqrt(6.0L * sum);
     }
 
     /*
         OMP EULER
     */
-    double pi_euler_omp(std::size_t iterations) {
-        double sum = 0.0;
+    long double pi_euler_omp( const std::size_t iterations) {
+        long double sum = 0.0;
 
         #pragma omp parallel for simd reduction(+:sum)
-        
         for (unsigned long long n = 1; n < iterations; ++n) {
-            const double d = static_cast<double>(n);
-            sum += 1.0 / (d * d);
+            const long double d = static_cast<long double>(n);
+            sum += 1.0L / (d * d);
         }
-        return std::sqrt(6.0 * sum);
+        return std::sqrt(6.0L * sum);
     }
 
     /*
     	GAUSSIAN
     */
-    double gaussian_integral(std::size_t iterations, double step) {
-        double result = 0.0;
-        double x = 0.0;
+    long double gaussian_integral( const std::size_t iterations, const long double step) {
+        long double result = 0.0;
+        long double x = 0.0;
         for (std::size_t k = 0; k < iterations; ++k) {
             result += std::exp(-(x * x)) * step;
-            x = static_cast<double>(k + 1) * step;
+            x = static_cast<long double>(k + 1) * step;
         }
-        result *= 2.0;
+        result *= 2.0L;
         return result * result;
     }
 
     /*
     	OMP GAUSSIAN
     */
-    double gaussian_integral_omp(std::size_t iterations, double step) {
-        double result = 0.0;
-        
+    long double gaussian_integral_omp( const std::size_t iterations, const long double step) {
+        long double result = 0.0L;
+        long double x = 0.0L;
         #pragma omp parallel for simd reduction(+:result)
         for (unsigned long long k = 0; k < iterations; ++k) {
-            double x = static_cast<double>(k + 1) * step;
             result += std::exp(-(x * x)) * step;
+            x = static_cast<long double>(k + 1) * step;
         }
-        result *= 2.0;
+        result *= 2.0L;
         return result * result;
     }
 
     /*
       Wallis
     */
-    double wallis( std::size_t iterations ) {
-        double sum = 0.0;
+    long double pi_wallis( const std::size_t iterations ) {
+        long double prod = 1.0L;
         for (unsigned long long k = 1; k < iterations; ++k) {
-            sum *= 4*k*k / (4*k*k-1);
+            prod *= 4*k*k / (4*k*k-1);
         } 
 
-        return 2.0*sum;
+        return 2.0L * prod;
     }
 
     /*
       Wallis OMP
     */
-    double wallis_omp( std::size_t iterations ) {
-        double sum = 0.0;
-        #pragma omp parallel for simd reduction(+:sum)
+    long double pi_wallis_omp( const std::size_t iterations ) {
+        long double prod = 1.0L;
+        #pragma omp parallel for simd reduction(+:prod)
         for (unsigned long long k = 1; k < iterations; ++k) {
-            sum *= 4*k*k / (4*k*k-1);
+            prod *= 4*k*k / (4*k*k-1);
         } 
 
-        return 2.0*sum;
+        return 2.0L * prod;
     }
 
 } // namespace calc
